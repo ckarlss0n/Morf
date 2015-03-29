61f31524da35917aee56141169cdd4f86a88417d
unknown <lagebergman@gmail.com>
2015-03-29 21:35:55 +0200

    add libGDX basic project structure

---
+++ desktop/src/edu/chl/morf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.chl.morf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.chl.morf.Morf;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Morf(), config);
+	}
+}
