b478d2050d0c1d713f327fa11f9799e693c31556
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-04-13 14:02:19 +0200

    Make screen classes implement Screen

--- core/src/edu/chl/morf/Screens/MainMenuScreen.java
+++ core/src/edu/chl/morf/Screens/MainMenuScreen.java
@@ -1,7 +1,44 @@
 package edu.chl.morf.Screens;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by Christoffer on 2015-04-13.
  */
-public class MainMenuScreen {
+public class MainMenuScreen implements Screen{
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
