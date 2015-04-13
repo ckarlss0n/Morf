a2451fb52a64e0bc670e13bf0844be3fe2d305ed
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-04-13 13:48:05 +0200

    Fix errors in Morf.java

--- core/src/edu/chl/morf/Morf.java
+++ core/src/edu/chl/morf/Morf.java
@@ -1,10 +1,16 @@
 package edu.chl.morf;
 
-<<<<<<< Updated upstream
-=======
 import com.badlogic.gdx.ApplicationAdapter;
->>>>>>> Stashed changes
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Morf extends Game {
+
+	@Override
+	public void create () {
+
+	}
+}
