c56697d1cbf7f9c0c98bf00795328aed995e64f7
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-04-13 14:47:55 +0200

    Set screen to GameScreen in Morf.java

--- core/src/edu/chl/morf/Morf.java
+++ core/src/edu/chl/morf/Morf.java
@@ -1,16 +1,16 @@
 package edu.chl.morf;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import edu.chl.morf.Screens.GameScreen;
 
 public class Morf extends Game {
 
 	@Override
 	public void create () {
-
+		setScreen(new GameScreen());
 	}
 }
