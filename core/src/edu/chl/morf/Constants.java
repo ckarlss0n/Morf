ee154bc2743657a5f3eda45bcb3e4d9c4cab1126
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-04-13 14:06:09 +0200

    Add Constants.java

---
+++ core/src/edu/chl/morf/Constants.java
@@ -0,0 +1,10 @@
+package edu.chl.morf;
+
+/**
+ * Created by Christoffer on 2015-04-13.
+ */
+public final class Constants {
+    public static final String GAME_NAME = "Morf";
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
+}
