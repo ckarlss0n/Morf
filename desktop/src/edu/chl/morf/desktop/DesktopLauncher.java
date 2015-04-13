d61f28d11734f3d65990fd6fb1b340ce0c9cc3e6
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-04-13 14:12:36 +0200

    Add game dimensions from Constants to DesktopLauncher

--- desktop/src/edu/chl/morf/desktop/DesktopLauncher.java
+++ desktop/src/edu/chl/morf/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package edu.chl.morf.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import edu.chl.morf.Morf;
+import static edu.chl.morf.Constants.*;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = GAME_WIDTH;
+		config.height = GAME_HEIGHT;
 		new LwjglApplication(new Morf(), config);
 	}
 }
