7bac8e62546081304da1d169f51ccb1976a25bd6
leigh <lagebergman@gmail.com>
2015-04-16 15:40:00 +0200

    Add main menu screen to Morf

--- core/src/edu/chl/morf/Morf.java
+++ core/src/edu/chl/morf/Morf.java
@@ -2,11 +2,26 @@ package edu.chl.morf;
 
 import com.badlogic.gdx.Game;
 import edu.chl.morf.Screens.GameScreen;
+import edu.chl.morf.Screens.MainMenuScreen;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class Morf extends Game {
 
+    PropertyChangeListener pcs;
+    GameScreen gameScreen;
+    MainMenuScreen mainMenuScreen;
 	@Override
 	public void create () {
-		setScreen(new GameScreen());
+        gameScreen = new GameScreen();
+        mainMenuScreen = new MainMenuScreen();
+        pcs = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+
+            }
+        };
+        setScreen(gameScreen);
 	}
 }
