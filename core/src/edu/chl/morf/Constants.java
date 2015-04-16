a5cacd8add27e36b975897d2cf6820104a76762c
leigh <lagebergman@gmail.com>
2015-04-16 14:21:01 +0200

    Change spritesheet name in Constants

--- core/src/edu/chl/morf/Constants.java
+++ core/src/edu/chl/morf/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     public static final int GAME_HEIGHT = 600;
     public static final Vector2 WORLD_GRAVITY = new Vector2(0,-15);
     public static final int MAX_SPEED = 5;
-    public static final String CHARACTERS_ATLAS_PATH = "SpriteSheets/RunningStickman";
+    public static final String CHARACTERS_ATLAS_PATH = "SpriteSheets/Stickman";
     public static final String[] RUNNER_RUNNING_REGION_NAMES = new String[] {"runningRight1", "runningRight2", "runningRight3",
             "runningLeft1", "runningLeft2", "runningLeft3", "idle"};
 }
