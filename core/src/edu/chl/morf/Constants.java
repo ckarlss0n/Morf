3a2f9e16440b4fa402829f0dc40a656004c05446
leigh <lagebergman@gmail.com>
2015-04-17 16:27:10 +0200

    Add button sprite sheet

--- core/src/edu/chl/morf/Constants.java
+++ core/src/edu/chl/morf/Constants.java
@@ -15,12 +15,19 @@ public final class Constants {
     //Game play constants
     public static final Vector2 WORLD_GRAVITY = new Vector2(0,-15);
     public static final int MAX_SPEED = 2;
+
+    //Character animation constants
     public static final String CHARACTERS_ATLAS_PATH = "SpriteSheets/Stickman";
     public static final String[] PLAYERCHARACTER_RUNNINGLEFT_REGION_NAMES = new String[] {"runningLeft1", "runningLeft2", "runningLeft3"};
     public static final String[] PLAYERCHARACTER_RUNNINGRIGHT_REGION_NAMES = new String[] {"runningRight1", "runningRight2", "runningRight3"};
+    public static final String PLAYERCHARACTER_IDLE_REGION_NAME = "idle";
+
+    //Button constants
+    public static final String BUTTONS_ATLAS_PATH = "SpriteSheets/Buttons";
+    public static final String BUTTON_UNPRESSED_REGION_NAME = "ButtonUnpressed";
+    public static final String BUTTON_PRESSED_REGION_NAME = "ButtonPressed";
 
     //Background constants
-    public static final String PLAYERCHARACTER_IDLE_REGION_NAME = "idle";
     public static final String BACKGROUND_IMAGE_PATH = "background.png";
     public static final String BOTTOM_CLOUDS_IMAGE_PATH = "cloudsBottom.png";
     public static final String TOP_CLOUDS_IMAGE_PATH = "cloudsTop.png";
