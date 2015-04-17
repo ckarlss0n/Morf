173a003337f9b6ce31333ebbd873f026c60e7cf3
leigh <lagebergman@gmail.com>
2015-04-17 13:25:26 +0200

    Add stage to MainMenuScreen

--- core/src/edu/chl/morf/Screens/MainMenuScreen.java
+++ core/src/edu/chl/morf/Screens/MainMenuScreen.java
@@ -1,9 +1,26 @@
 package edu.chl.morf.Screens;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import javax.swing.*;
+
 /**
  * Created by Christoffer on 2015-04-13.
  */
 public class MainMenuScreen extends ObservableScreen{
+
+    private Stage stage;
+
+    public MainMenuScreen(){
+        super();
+    }
+
+
+    @Override
+    public void render(float delta) {
+        stage.act(delta);
+        stage.draw();
+    }
 
     public void showGame(){
         try {
