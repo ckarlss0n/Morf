af0200b4b2541bc75103fef4ff6035520e80e200
leigh <lagebergman@gmail.com>
2015-04-28 09:16:45 +0200

    Add model package and new class Water in model

---
+++ core/src/edu/chl/morf/model/Water.java
@@ -0,0 +1,20 @@
+package edu.chl.morf.model;
+
+import java.awt.*;
+
+/**
+ * Created by Lage on 2015-04-28.
+ */
+public class Water {
+
+    private Point position;
+
+    public Water(){
+
+    }
+
+    public Point getPosition(){
+        return this.position;
+    }
+
+}
