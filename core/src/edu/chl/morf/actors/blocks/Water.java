d8da91c8fdc3a8bd240ed342fce8eb2095373284
leigh <lagebergman@gmail.com>
2015-04-29 19:22:15 +0200

    Modify draw method in Image Water

--- core/src/edu/chl/morf/actors/blocks/Water.java
+++ core/src/edu/chl/morf/actors/blocks/Water.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import edu.chl.morf.model.WaterState;
 
 /**
  * This class represents water.
@@ -40,11 +41,11 @@ public class Water extends Image{
         super.draw(batch, parentAlpha);
 
         //Draw current water state texture at water position
-        if(water.toString().equals("liquid")) {
+        if(water.getState() == WaterState.LIQUID) {
             //batch.draw(waterTexture, water.getPosition().x, water.getPosition().y, 30 / 100f, 30 / 100f);
-        }else if(water.toString().equals("gas")) {
+        }else if(water.getState() == WaterState.GAS) {
             //batch.draw(vaporTexture, water.getPosition().x, water.getPosition().y, 30 / 100f, 30 / 100f);
-        }else if(water.toString().equals("solid")) {
+        }else if(water.getState() == WaterState.SOLID) {
             //batch.draw(iceTexture, water.getPosition().x, water.getPosition().y, 30 / 100f, 30 / 100f);
         }
     }
