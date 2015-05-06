c55dc4d1bcf70b15150701208e12e552e616bc42
leigh <lagebergman@gmail.com>
2015-05-06 15:37:31 +0200

    Add new class View

---
+++ core/src/edu/chl/morf/view/View.java
@@ -0,0 +1,15 @@
+package edu.chl.morf.view;
+
+import edu.chl.morf.model.Level;
+
+/**
+ * Created by Lage on 2015-05-06.
+ */
+public class View {
+
+    private Level level;
+
+    public void render(){
+
+    }
+}
