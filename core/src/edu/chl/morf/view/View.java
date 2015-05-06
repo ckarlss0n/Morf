afe4a26956507231f619b44b42da18e2c9393bfd
leigh <lagebergman@gmail.com>
2015-05-06 15:41:11 +0200

    Add SpriteBatch for drawing textures in View

--- core/src/edu/chl/morf/view/View.java
+++ core/src/edu/chl/morf/view/View.java
@@ -1,6 +1,11 @@
 package edu.chl.morf.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import edu.chl.morf.model.Level;
+import edu.chl.morf.model.TileType;
 
 /**
  * Created by Lage on 2015-05-06.
@@ -8,8 +13,24 @@ import edu.chl.morf.model.Level;
 public class View {
 
     private Level level;
+    private Batch batch;
+
+    public View(){
+        batch = new SpriteBatch();
+    }
 
     public void render(){
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //Clears the screen.
+        batch.begin();
+        for(TileType[] tileRow: level.getMatrix()){
+            for(TileType tile: tileRow){
+                switch (tile){
+                    case FLOWER:
 
+                        break;
+                }
+            }
+        }
+        batch.end();
     }
 }
