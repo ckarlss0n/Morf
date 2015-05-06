fa06fd36083b514a8065456f474f4fd6e98d6277
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-06 15:07:10 +0200

    Add ScreenManager for handling screens

---
+++ core/src/edu/chl/morf/handlers/ScreenManager.java
@@ -0,0 +1,56 @@
+package edu.chl.morf.handlers;
+
+import java.util.Stack;
+
+import edu.chl.morf.main.Main;
+import edu.chl.morf.screens2.GameScreen;
+import edu.chl.morf.screens2.PlayScreen;
+
+public class ScreenManager {
+	
+	private Main game;
+	
+	private Stack<GameScreen> screens;
+	
+	public static final int PLAY = 656987;
+	
+	public ScreenManager(Main game){
+		this.game = game;
+		screens = new Stack<GameScreen>();
+		pushState(PLAY);
+	}
+	
+	public Main getGame(){
+		return game;
+	}
+	
+	public void update(float dt){
+		screens.peek().update(dt);
+	}
+	
+	public void render(float dt){
+		screens.peek().render(dt);
+	}
+	
+	private GameScreen getScreen(int screen){
+		if(screen == PLAY){
+			return new PlayScreen(this);
+		}
+		return null;
+	}
+	
+	public void setState(int screen){
+		popScreen();
+		pushState(screen);
+	}
+	
+	public void pushState(int screen){
+		screens.push(getScreen(screen));
+	}
+	
+	public void popScreen(){
+		GameScreen gs = screens.pop();
+		gs.dispose();
+	}
+	
+}
