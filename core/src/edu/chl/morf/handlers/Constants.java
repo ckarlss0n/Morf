6298034f1b4360dec748f4153ca74c11741e2329
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-06 15:06:36 +0200

    Add Constants in right package

---
+++ core/src/edu/chl/morf/handlers/Constants.java
@@ -0,0 +1,16 @@
+package edu.chl.morf.handlers;
+
+public class Constants {
+	
+	//Pixels per meter
+	public static final float PPM = 100;
+	
+	//Category bits
+	public static final short BIT_GROUND = 2;
+	public static final short BIT_SPIKES = 4;
+	public static final short BIT_PLAYER = 8;
+	public static final short BIT_WATER = 8;
+	public static final short BIT_ICE = 8;
+	public static final short BIT_GAS = 8;
+	
+}
