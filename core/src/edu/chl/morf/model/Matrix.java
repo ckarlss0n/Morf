50a064c70e17777861f7d667b6366b237ad241b0
leigh <lagebergman@gmail.com>
2015-05-07 11:22:09 +0200

    Add Matrix class in model package

---
+++ core/src/edu/chl/morf/model/Matrix.java
@@ -0,0 +1,15 @@
+package edu.chl.morf.model;
+
+/**
+ * Created by Lage on 2015-05-07.
+ */
+public class Matrix {
+
+    private int maxRows;
+    private int maxColumns;
+
+    public Matrix(int maxRows, int maxColumns){
+        this.maxRows = maxRows;
+        this.maxColumns = maxColumns;
+    }
+}
