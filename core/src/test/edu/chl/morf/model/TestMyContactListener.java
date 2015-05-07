48c5dc2820c5418641eb3a5ee04ccbef707e5b82
Harald Brorsson <harald.brorsson@gmail.com>
2015-05-07 15:45:28 +0200

    Create TestMyContactListener

--- core/src/test/edu/chl/morf/model/TestMyContactListener.java
+++ core/src/test/edu/chl/morf/model/TestMyContactListener.java
@@ -1,7 +1,18 @@
 package test.edu.chl.morf.model;
 
+import com.badlogic.gdx.physics.box2d.Contact;
+import org.junit.Test;
+
 /**
- * Created by chrx on 5/7/15.
+ * Created by Harald Brorsson on 5/7/15.
  */
 public class TestMyContactListener {
+    @Test
+    public void beginContact(Contact contact){
+
+    }
+    @Test
+    public void endContact(Contact contact){
+
+    }
 }
