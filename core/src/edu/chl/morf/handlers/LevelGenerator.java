207d80f7197d8bbebfe14792779d24c6fe14725d
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-07 14:13:23 +0200

    Add LevelGenerator

---
+++ core/src/edu/chl/morf/handlers/LevelGenerator.java
@@ -0,0 +1,63 @@
+package edu.chl.morf.handlers;
+
+import static edu.chl.morf.Constants.LEVEL_PATH;
+import static edu.chl.morf.handlers.Constants.*;
+import static edu.chl.morf.Constants.GROUND_FRICTION;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+import edu.chl.morf.model.Level;
+
+public class LevelGenerator {
+
+    private TiledMapTileLayer groundLayer;
+    private float tileSize;
+
+	public void generateLevel(Level level, World world){
+		
+		TiledMap tileMap = new TmxMapLoader().load(LEVEL_PATH + level.getName());
+		groundLayer = (TiledMapTileLayer) tileMap.getLayers().get("Tile Layer 1");
+		
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.fixedRotation = true;
+		FixtureDef fixDef = new FixtureDef();
+
+		for (int row = 0; row < level.getMatrix().getRows(); row++) {
+			for (int col = 0; col < level.getMatrix().getColumns(); col++) {
+				TiledMapTileLayer.Cell cell = groundLayer.getCell(col, row);
+
+
+				if (cell == null) continue;
+				if (cell.getTile() == null) continue;
+
+				bodyDef.type = BodyType.StaticBody;
+				bodyDef.position.set((col + 0.5f) * tileSize / PPM, (row + 0.5f) * tileSize / PPM);
+
+				ChainShape chainShape = new ChainShape();
+				Vector2[] v = new Vector2[5];
+				v[0] = new Vector2(-tileSize / 2 / PPM, -tileSize / 2 / PPM);
+				v[1] = new Vector2(-tileSize / 2 / PPM, tileSize / 2 / PPM);
+				v[2] = new Vector2(tileSize / 2 / PPM, tileSize / 2 / PPM);
+				v[3] = new Vector2(tileSize / 2 / PPM, -tileSize / 2 / PPM);
+				v[4] = new Vector2(-tileSize / 2 / PPM, -tileSize / 2 / PPM);
+
+				chainShape.createChain(v);
+				fixDef.friction = GROUND_FRICTION;
+				fixDef.shape = chainShape;
+				fixDef.filter.categoryBits = BIT_GROUND;
+				fixDef.filter.maskBits = -1;
+				fixDef.isSensor = false;
+				world.createBody(bodyDef).createFixture(fixDef);
+			}
+		}
+
+	}
+}
