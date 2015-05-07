14cbdc8c3a62526f9a130745a065f96b11256a0a
leigh <lagebergman@gmail.com>
2015-05-07 11:50:16 +0200

    Add list of LevelObjects in Matrix class

--- core/src/edu/chl/morf/model/Matrix.java
+++ core/src/edu/chl/morf/model/Matrix.java
@@ -1,5 +1,7 @@
 package edu.chl.morf.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by Lage on 2015-05-07.
  */
@@ -7,9 +9,23 @@ public class Matrix {
 
     private int maxRows;
     private int maxColumns;
+    private ArrayList<LevelObject> objects;
 
     public Matrix(int maxRows, int maxColumns){
         this.maxRows = maxRows;
         this.maxColumns = maxColumns;
+    }
+
+    public Matrix(int maxRows, int maxColumns, ArrayList<LevelObject> objects){
+        this(maxRows,maxColumns);
+        this.objects = objects;
+    }
+
+    public void addLevelObject(LevelObject object){
+        int x = object.getPosition().x;
+        int y = object.getPosition().y;
+        if(x >= 0 && x <= maxColumns && y >= 0 && y <= maxRows){
+            this.objects.add(object);
+        }
     }
 }
