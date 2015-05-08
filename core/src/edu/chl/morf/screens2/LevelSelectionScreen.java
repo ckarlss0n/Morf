dd2dcf28957cfa247b3e4e49f025173a4a8363c3
leigh <lagebergman@gmail.com>
2015-05-08 17:13:00 +0200

    Add new class LevelSelectionScreen

---
+++ core/src/edu/chl/morf/screens2/LevelSelectionScreen.java
@@ -0,0 +1,58 @@
+package edu.chl.morf.screens2;
+
+import edu.chl.morf.handlers.ScreenManager;
+
+/**
+ * Created by Lage on 2015-05-08.
+ */
+public class LevelSelectionScreen extends GameScreen{
+
+    public LevelSelectionScreen(ScreenManager sm) {
+        super(sm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
