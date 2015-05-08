8e73f0a0a4ff839c14d8df82045c924840ba43f1
leigh <lagebergman@gmail.com>
2015-05-08 17:15:34 +0200

    Make LevelSelectionScreen implement EventListener

--- core/src/edu/chl/morf/screens2/LevelSelectionScreen.java
+++ core/src/edu/chl/morf/screens2/LevelSelectionScreen.java
@@ -1,11 +1,15 @@
 package edu.chl.morf.screens2;
 
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import edu.chl.morf.handlers.ScreenManager;
 
 /**
  * Created by Lage on 2015-05-08.
  */
-public class LevelSelectionScreen extends GameScreen{
+public class LevelSelectionScreen extends GameScreen implements EventListener{
 
     public LevelSelectionScreen(ScreenManager sm) {
         super(sm);
@@ -54,5 +58,24 @@ public class LevelSelectionScreen extends GameScreen{
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean handle(Event event) {
+        if(event instanceof ChangeListener.ChangeEvent){
+            ChangeListener.ChangeEvent changeEvent = (ChangeListener.ChangeEvent)event;
+            if(changeEvent.getTarget() instanceof TextButton){
+                TextButton button = (TextButton)changeEvent.getTarget();
+                String buttonText = button.getLabel().getText().toString();
+                if(buttonText.equals("PLAY")){
+                    /*sm.setStageInputHandler();
+                    gameScreen.show();
+                    setScreen(gameScreen);*/
+                }else if(buttonText.equals("SETTINGS")){
+
+                }
+            }
+        }
+        return true;
     }
 }
