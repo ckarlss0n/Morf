9d8238cad7b37bb7d1e283f2fbc70cd66fe32ebd
leigh <lagebergman@gmail.com>
2015-05-17 16:33:54 +0200

    Make HighScoreHandler extend FileHandler

--- core/src/edu/chl/morf/handlers/HighScoreHandler.java
+++ core/src/edu/chl/morf/handlers/HighScoreHandler.java
@@ -9,15 +9,9 @@ import java.util.Map;
 /**
  * Created by Lage on 2015-05-12.
  */
-public class HighScoreHandler {
-    private BufferedReader bufferedReader;
-    PrintWriter printWriter;
-    private Map<Level,Integer> highScores;
+public class HighScoreHandler extends FileHandler{
+    private Map<Level,Integer> highScores = new HashMap<Level, Integer>();
     private static final HighScoreHandler instance = new HighScoreHandler();
-
-    private HighScoreHandler(){
-        highScores = new HashMap<Level, Integer>();
-    }
 
     public static HighScoreHandler getInstance(){
         return instance;
@@ -31,51 +25,30 @@ public class HighScoreHandler {
         return highScores.get(level);
     }
 
-    public void writeHighScores(){
-        try{
-            printWriter = new PrintWriter(System.getProperty("user.home") + "/.morf/.highscore.txt");
-            for(Level level : highScores.keySet()){
-                printWriter.println(level.getName() + ";" + highScores.get(level).toString());
-            }
-        }catch(FileNotFoundException e){
-            System.out.println("File not found");
+    @Override
+    public void write(PrintWriter printWriter) throws FileNotFoundException{
+        for(Level level : highScores.keySet()){
+            printWriter.println(level.getName() + ";" + highScores.get(level).toString());
         }
-        printWriter.close();
     }
 
-    public void readHighScores(){
-        try{
-            //Trys to read file
-            bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.home") + "/.morf/.highscore.txt"));
-        }catch (FileNotFoundException e){
-            //Creates file if file not found
-            File f = new File(System.getProperty("user.home") + "/.morf/.highscore.txt");
-            f.getParentFile().mkdirs();
-            try {
-                f.createNewFile();
-            } catch (IOException ex) {
-
-            }
+    @Override
+    public void read(BufferedReader bufferedReader) throws IOException{
+        LevelFactory levelFactory = new LevelFactory();
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            String[] lineSplit = line.split(";");
+            String levelName = lineSplit[0];
+            String highScoreString = lineSplit[1];
+            Integer highScore = Integer.parseInt(highScoreString);
+            Level level = levelFactory.getLevel(levelName);
+            highScores.put(level, highScore);
+            line = bufferedReader.readLine();
         }
+    }
 
-        try{
-            bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.home") + "/.morf/.highscore.txt"));
-            String line = bufferedReader.readLine();
-            LevelFactory levelFactory = new LevelFactory();
-            while (line != null) {
-                String[] lineSplit = line.split(";");
-                String levelName = lineSplit[0];
-                String highScoreString = lineSplit[1];
-                Integer highScore = Integer.parseInt(highScoreString);
-                Level level = levelFactory.getLevel(levelName);
-                highScores.put(level, highScore);
-                line = bufferedReader.readLine();
-            }
-            bufferedReader.close();
-        }catch(FileNotFoundException e){
-            System.out.println("File not found");
-        } catch (IOException ex) {
-            System.out.println("IO Exception");
-        }
+    @Override
+    public String getFilePath() {
+        return "/.morf/.highscore.txt";
     }
 }
