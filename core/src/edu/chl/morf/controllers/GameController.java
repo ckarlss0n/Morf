df16f9fe07a185cdcecb26d0195e1e42fb32d82e
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-18 14:36:59 +0200

    Make GameController use keys from KeyBindings

--- core/src/edu/chl/morf/controllers/GameController.java
+++ core/src/edu/chl/morf/controllers/GameController.java
@@ -3,6 +3,7 @@ package edu.chl.morf.controllers;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import edu.chl.morf.handlers.KeyBindings;
 
 /**
  * Created by Christoffer on 2015-05-06.
@@ -10,66 +11,55 @@ import com.badlogic.gdx.InputAdapter;
 public class GameController extends InputAdapter{
 
 	private GameLogic gameLogic;
+	private KeyBindings keyBindings = KeyBindings.getInstance();
 
 	public GameController(GameLogic gameLogic){
 		this.gameLogic = gameLogic;
 		Gdx.input.setInputProcessor(this);
 	}
 
+	private boolean isKey(int keyCode, String keyName){
+		return keyCode == Input.Keys.valueOf(keyName);
+	}
+
 	@Override
 	public boolean keyDown(int keycode) {
-		switch (keycode) {
-			case Input.Keys.LEFT:
-				gameLogic.setKeyState(keycode, true);
-				gameLogic.moveLeft();
-				break;
-			case Input.Keys.RIGHT:
-				gameLogic.setKeyState(keycode, true);
-				gameLogic.moveRight();
-				break;
-			case Input.Keys.UP:
-				gameLogic.setKeyState(keycode, true);
-				gameLogic.jump();
-				break;
-			case Input.Keys.SPACE:
-				gameLogic.resetGame();
-				break;
-			case Input.Keys.SHIFT_LEFT:
-				gameLogic.fly();
-				break;
-			case Input.Keys.X:
-				gameLogic.placeWater();
-				break;
-			case Input.Keys.C:
-				gameLogic.heatBlock();
-				break;
-			case Input.Keys.Z:
-				gameLogic.coolBlock();
-				break;
+		if (isKey(keycode, keyBindings.getMoveLeftKey())) {
+			gameLogic.setKeyState(keycode, true);
+			gameLogic.moveLeft();
+		} else if (isKey(keycode, keyBindings.getMoveRightKey())){
+			gameLogic.setKeyState(keycode, true);
+			gameLogic.moveRight();
+		} else if(isKey(keycode, keyBindings.getJumpKey())) {
+			gameLogic.setKeyState(keycode, true);
+			gameLogic.jump();
+		} else if(isKey(keycode, keyBindings.getFlyKey())) {
+			gameLogic.fly();
+		} else if(isKey(keycode, keyBindings.getPourKey())) {
+			gameLogic.placeWater();
+		} else if(isKey(keycode, keyBindings.getHeatKey())) {
+			gameLogic.heatBlock();
+		} else if(isKey(keycode, keyBindings.getCoolKey())) {
+			gameLogic.coolBlock();
 		}
 		return true;
 	}
 
 	@Override
 	public boolean keyUp(int keycode) {
-		switch (keycode) {
-			case Input.Keys.LEFT:
-				gameLogic.setKeyState(Input.Keys.LEFT, false);
-				gameLogic.moveLeft();
-				gameLogic.stop();
-				break;
-			case Input.Keys.RIGHT:
-				gameLogic.setKeyState(Input.Keys.RIGHT, false);
-				gameLogic.moveRight();
-				gameLogic.stop();
-				break;
-			case Input.Keys.UP:
-				gameLogic.setKeyState(Input.Keys.UP, false);
-				gameLogic.stop();
-				break;
-			case Input.Keys.SHIFT_LEFT:
-				gameLogic.stop();
-				break;
+		if(isKey(keycode, keyBindings.getMoveLeftKey())) {
+			gameLogic.setKeyState(Input.Keys.valueOf(keyBindings.getMoveLeftKey()), false);
+			gameLogic.moveLeft();
+			gameLogic.stop();
+		} else if(isKey(keycode, keyBindings.getMoveRightKey())) {
+			gameLogic.setKeyState(Input.Keys.valueOf(keyBindings.getMoveRightKey()), false);
+			gameLogic.moveRight();
+			gameLogic.stop();
+		} else if(isKey(keycode, keyBindings.getJumpKey())) {
+			gameLogic.setKeyState(Input.Keys.valueOf(keyBindings.getJumpKey()), false);
+			gameLogic.stop();
+		} else if(isKey(keycode, keyBindings.getFlyKey())) {
+			gameLogic.stop();
 		}
 		return true;
 	}
