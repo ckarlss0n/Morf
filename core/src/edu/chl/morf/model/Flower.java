ef866d3bfbe081424eaf3134ffeac3bd5638b954
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-18 13:50:01 +0200

    Add position to Flower

--- core/src/edu/chl/morf/model/Flower.java
+++ core/src/edu/chl/morf/model/Flower.java
@@ -1,9 +1,22 @@
 package edu.chl.morf.model;
 
+import java.awt.geom.Point2D;
+
 /**
  * Created by Lage on 2015-04-29.
  */
 public class Flower implements Block{
+	
+	private Point2D.Float position;
+	
+	public Flower(Point2D.Float position){
+		this.position = position;
+	}
+	
+	public Point2D.Float getPosition(){
+		return position;
+	}
+	
     @Override
     public void heat() {
 
