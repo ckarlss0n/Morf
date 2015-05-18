608175b480711b5019cd33c73ee7aba4e677b1e3
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-18 14:44:57 +0200

    Make SettingsHandler load and save key bindings

--- core/src/edu/chl/morf/handlers/SettingsHandler.java
+++ core/src/edu/chl/morf/handlers/SettingsHandler.java
@@ -14,40 +14,67 @@ public class SettingsHandler extends FileHandler{
 
     public void write(PrintWriter printWriter){
         SoundHandler soundHandler = SoundHandler.getInstance();
+        KeyBindings keyBindings = KeyBindings.getInstance();
+
+        //Sound preferences
         printWriter.println(soundHandler.getMusicVolume());
         printWriter.println(soundHandler.getSoundEffectsVolume());
         printWriter.println(soundHandler.isMusicEnabled());
         printWriter.println(soundHandler.isSoundEffectsEnabled());
+
+        //Key bindings
+        printWriter.println(keyBindings.getMoveLeftKey());
+        printWriter.println(keyBindings.getMoveRightKey());
+        printWriter.println(keyBindings.getJumpKey());
+        printWriter.println(keyBindings.getFlyKey());
+        printWriter.println(keyBindings.getPourKey());
+        printWriter.println(keyBindings.getCoolKey());
+        printWriter.println(keyBindings.getHeatKey());
     }
 
     @Override
     public void read(BufferedReader bufferedReader) throws IOException {
-        SoundHandler soundHandler = SoundHandler.getInstance();
-        String line = bufferedReader.readLine();
-        if(line != null) {
-            try {
-                soundHandler.setMusicVolume(Float.parseFloat(line));
-            } catch (NumberFormatException e) {
-                System.out.println("Unable to parse string");
+        KeyBindings keyBindings = KeyBindings.getInstance();
+        try {
+            SoundHandler soundHandler = SoundHandler.getInstance();
+            String line = bufferedReader.readLine();
+            if (line != null) {
+                try {
+                    soundHandler.setMusicVolume(Float.parseFloat(line));
+                } catch (NumberFormatException e) {
+                    System.out.println("Unable to parse string");
+                }
+                line = bufferedReader.readLine();
+                try {
+                    soundHandler.setSoundEffectsVolume(Float.parseFloat(line));
+                } catch (NumberFormatException e) {
+                    System.out.println("Unable to parse string");
+                }
+                line = bufferedReader.readLine();
+                if (line.equals("true")) {
+                    soundHandler.enableMusic();
+                } else {
+                    soundHandler.muteMusic();
+                }
+                line = bufferedReader.readLine();
+                if (line.equals("true")) {
+                    soundHandler.enabledSoundEffects();
+                } else {
+                    soundHandler.muteSoundEffects();
+                }
+
+                //Key bindings
+                keyBindings.setMoveLeftKey(bufferedReader.readLine());
+                keyBindings.setMoveRightKey(bufferedReader.readLine());
+                keyBindings.setJumpKey(bufferedReader.readLine());
+                keyBindings.setFlyKey(bufferedReader.readLine());
+                keyBindings.setPourKey(bufferedReader.readLine());
+                keyBindings.setCoolKey(bufferedReader.readLine());
+                keyBindings.setHeatKey(bufferedReader.readLine());
+                keyBindings.bindKeys();
             }
-            line = bufferedReader.readLine();
-            try {
-                soundHandler.setSoundEffectsVolume(Float.parseFloat(line));
-            } catch (NumberFormatException e) {
-                System.out.println("Unable to parse string");
-            }
-            line = bufferedReader.readLine();
-            if (line.equals("true")) {
-                soundHandler.enableMusic();
-            } else {
-                soundHandler.muteMusic();
-            }
-            line = bufferedReader.readLine();
-            if (line.equals("true")) {
-                soundHandler.enabledSoundEffects();
-            } else {
-                soundHandler.muteSoundEffects();
-            }
+        } catch (NullPointerException e){
+            keyBindings.bindKeys();
         }
     }
 
