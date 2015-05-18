e85acce356a92b44806856777b7d639fe5d6e962
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-18 11:13:37 +0200

    Add code for checking water state when creating water bodies

--- core/src/edu/chl/morf/handlers/LevelGenerator.java
+++ core/src/edu/chl/morf/handlers/LevelGenerator.java
@@ -21,6 +21,7 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 import edu.chl.morf.model.Level;
+import edu.chl.morf.model.LevelObject;
 import edu.chl.morf.model.Water;
 import edu.chl.morf.model.WaterState;
 
@@ -39,6 +40,28 @@ public class LevelGenerator {
 		FixtureDef fixDef = new FixtureDef();
 		BodyFactory bodyFactory = new BodyFactory();
 
+		//Generate ground bodies
+//		for (LevelObject object : level.getMatrix().getLevelObjects()){
+//			bodyDef.type = BodyType.StaticBody;
+//			bodyDef.position.set((col + 0.5f) * TILE_SIZE / PPM, (row + 0.5f) * TILE_SIZE / PPM);
+//
+//			ChainShape chainShape = new ChainShape();
+//			Vector2[] v = new Vector2[5];
+//			v[0] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+//			v[1] = new Vector2(-TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
+//			v[2] = new Vector2(TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
+//			v[3] = new Vector2(TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+//			v[4] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+//
+//			chainShape.createChain(v);
+//			fixDef.friction = GROUND_FRICTION;
+//			fixDef.shape = chainShape;
+//			fixDef.filter.categoryBits = BIT_GROUND;
+//			fixDef.filter.maskBits = -1;
+//			fixDef.isSensor = false;
+//			world.createBody(bodyDef).createFixture(fixDef);
+//		}
+		
 		for (int row = 0; row < level.getMatrix().getRows(); row++) {
 			for (int col = 0; col < level.getMatrix().getColumns(); col++) {
 				TiledMapTileLayer.Cell cell = groundLayer.getCell(col, row);
@@ -67,12 +90,23 @@ public class LevelGenerator {
 				world.createBody(bodyDef).createFixture(fixDef);
 			}
 		}
+		
+		//Generate spike bodies
 
+		//Generate water bodies
 		for (Water water : level.getWaterBlocks()) {
-			
-			Body body = bodyFactory.createWaterBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
-			bodyBlockMap.put(body, water);
+			if (water.getState() == WaterState.LIQUID){
+				Body body = bodyFactory.createWaterBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
+				bodyBlockMap.put(body, water);
+			}
+//			else if (water.getState() == WaterState.SOLID){
+//				Body body = bodyFactory.createIceBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
+//				bodyBlockMap.put(body, water);
+//			}
+//			else if (water.getState() == WaterState.GAS){
+//				Body body = bodyFactory.createVaporBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
+//				bodyBlockMap.put(body, water);
+//			}
 		}
-
 	}
 }
