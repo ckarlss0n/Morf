9101ecad72fc828eef0d118d122a3251e3dedd6f
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-18 14:26:43 +0200

    Generate bodies for spikes and flower

--- core/src/edu/chl/morf/handlers/LevelGenerator.java
+++ core/src/edu/chl/morf/handlers/LevelGenerator.java
@@ -20,8 +20,10 @@ import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
+import edu.chl.morf.model.Flower;
 import edu.chl.morf.model.Level;
 import edu.chl.morf.model.LevelObject;
+import edu.chl.morf.model.TileType;
 import edu.chl.morf.model.Water;
 import edu.chl.morf.model.WaterState;
 import static edu.chl.morf.handlers.LevelFactory.TILE_SIZE;
@@ -35,30 +37,48 @@ public class LevelGenerator {
 		FixtureDef fixDef = new FixtureDef();
 		BodyFactory bodyFactory = new BodyFactory();
 		
-		//Generate ground bodies
+		//Generate ground and spike bodies
 		for (LevelObject object : level.getMatrix().getLevelObjects()){
-			bodyDef.type = BodyType.StaticBody;
-			bodyDef.position.set((object.getPosition().y + 0.5f) * TILE_SIZE / PPM, (object.getPosition().x + 0.5f) * TILE_SIZE / PPM);
+			if (object.getTileType() == TileType.GROUND){
+				bodyDef.type = BodyType.StaticBody;
+				bodyDef.position.set((object.getPosition().y + 0.5f) * TILE_SIZE / PPM, (object.getPosition().x + 0.5f) * TILE_SIZE / PPM);
 
-			ChainShape chainShape = new ChainShape();
-			Vector2[] v = new Vector2[5];
-			v[0] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
-			v[1] = new Vector2(-TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
-			v[2] = new Vector2(TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
-			v[3] = new Vector2(TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
-			v[4] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				ChainShape chainShape = new ChainShape();
+				Vector2[] v = new Vector2[5];
+				v[0] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				v[1] = new Vector2(-TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
+				v[2] = new Vector2(TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
+				v[3] = new Vector2(TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				v[4] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
 
-			chainShape.createChain(v);
-			fixDef.friction = GROUND_FRICTION;
-			fixDef.shape = chainShape;
-			fixDef.filter.categoryBits = BIT_GROUND;
-			fixDef.filter.maskBits = -1;
-			fixDef.isSensor = false;
-			world.createBody(bodyDef).createFixture(fixDef);
+				chainShape.createChain(v);
+				fixDef.friction = GROUND_FRICTION;
+				fixDef.shape = chainShape;
+				fixDef.filter.categoryBits = BIT_GROUND;
+				fixDef.filter.maskBits = -1;
+				fixDef.isSensor = false;
+				world.createBody(bodyDef).createFixture(fixDef);
+			}
+			else if (object.getTileType() == TileType.SPIKES){
+				bodyDef.type = BodyType.StaticBody;
+				bodyDef.position.set((object.getPosition().y + 0.5f) * TILE_SIZE / PPM, (object.getPosition().x + 0.5f) * TILE_SIZE / PPM);
+
+				ChainShape chainShape = new ChainShape();
+				Vector2[] v = new Vector2[5];
+				v[0] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				v[1] = new Vector2(-TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
+				v[2] = new Vector2(TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
+				v[3] = new Vector2(TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				v[4] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+
+				chainShape.createChain(v);
+				fixDef.shape = chainShape;
+				fixDef.filter.categoryBits = BIT_SPIKES;
+				fixDef.filter.maskBits = BIT_PLAYER;
+				fixDef.isSensor = false;
+				world.createBody(bodyDef).createFixture(fixDef);
+			}
 		}
-		
-		
-		//Generate spike bodies
 
 		//Generate water bodies
 		for (Water water : level.getWaterBlocks()) {
@@ -75,5 +95,9 @@ public class LevelGenerator {
 //				bodyBlockMap.put(body, water);
 //			}
 		}
+		
+		//Generate flower body
+//		Flower flower = level.getFlower();
+//		bodyFactory.createFlowerBody(world, new Vector2(flower.getPosition().x, flower.getPosition().y));
 	}
 }
