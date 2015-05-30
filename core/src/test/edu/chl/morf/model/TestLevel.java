615b3eabafeed8b9edd482af7eb143bb3ec47ef3
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-30 23:46:18 +0200

    Complete TestLevel

--- core/src/test/edu/chl/morf/model/TestLevel.java
+++ core/src/test/edu/chl/morf/model/TestLevel.java
@@ -2,26 +2,37 @@ package test.edu.chl.morf.model;
 
 import static org.junit.Assert.*;
 
-import java.awt.Point;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
+import edu.chl.morf.model.Flower;
 import edu.chl.morf.model.Level;
-import edu.chl.morf.model.PlayerCharacterModel;
+import edu.chl.morf.model.PlayerCharacter;
 import edu.chl.morf.model.Water;
 
 public class TestLevel {
 	@Test
-	public void testAddWater(){
-		Level level = new Level(null, null, null, null, null);
-		level.addWater(new Water(0, 0));
+	public void testAddAndRemoveWater(){
+		Level level = new Level(null, null, null, new ArrayList<Water>(), null);
+		Water water = new Water();
+		level.addWater(water);
 		assertTrue(level.getWaterBlocks().size() == 1);
+		level.removeWater(water);
+		assertTrue(level.getWaterBlocks().isEmpty());
 	}
 	
 	@Test
 	public void testPourWater(){
-		Level level = new Level(null, null, new PlayerCharacterModel(), null, null);
+		Level level = new Level(null, null, new PlayerCharacter(10, 10), new ArrayList<Water>(), null);
+		level.getPlayer().setGhostEmpty(false);
 		level.pourWater();
-		assertTrue(level.getWaterBlocks().get(0).getPosition().equals(level.getPlayer().getPosition()));
+		assertTrue(level.getWaterBlocks().isEmpty());
+		level.getPlayer().setGhostEmpty(true);
+		level.pourWater();
+		assertTrue(level.getWaterBlocks().size() == 1);
+		level.getPlayer().setActiveBlock(new Flower());
+		level.pourWater();
+		assertTrue(level.isLevelWon());
 	}
 }
