7fbc26d9222b23042080938792ad234f57d6cfa7
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-30 17:43:23 +0200

    Add documentation to ScreenManager

--- core/src/edu/chl/morf/handlers/ScreenManager.java
+++ core/src/edu/chl/morf/handlers/ScreenManager.java
@@ -1,10 +1,14 @@
 package edu.chl.morf.handlers;
 
-import edu.chl.morf.main.Main;
 import edu.chl.morf.screens2.*;
 
 import java.util.Stack;
 
+/**
+ * A singleton class managing the different screens of the game.
+ * All screens are stored in a stack.
+ * The screen on top of the stack is the one to get rendered.
+ */
 public class ScreenManager {
 
 	private Stack<GameScreen> screens;
@@ -45,11 +49,12 @@ public class ScreenManager {
 			return new OptionsScreen(this);
 		}
 		else if(screen == ScreenType.PAUSE_SCREEN){
-			return new PauseScreen(this, screens.peek()); //Set current screen behind PauseScreen
+			return new PauseScreen(this, screens.peek()); //Show and render current screen (PlayScreen most likely) behind PauseScreen
 		}
 		return null;
 	}
-	
+
+	//TODO Remove?
 	public void setScreen(ScreenType screen, String levelName){
 		clearScreens();
 		pushScreen(screen, levelName);
@@ -58,17 +63,19 @@ public class ScreenManager {
 	public void pushScreen(ScreenType screen, String levelName){
 		screens.push(getScreen(screen, levelName));
 	}
-	
+
+	//Pop the screen on top of the stack, to return to the previously shown screen
 	public void popScreen(){
 		GameScreen gs = screens.pop();
 		gs.dispose();
 		screens.peek().setFocus();
+		//Resume game if the screen that appears after pop is PlayScreen
 		if(screens.peek() instanceof PlayScreen){
 			((PlayScreen)screens.peek()).resumeGame();
 		}
-
 	}
-	
+
+	//TODO Remove?
 	public void clearScreens(){
 		screens.clear();
 	}
