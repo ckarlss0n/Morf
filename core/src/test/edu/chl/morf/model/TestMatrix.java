9d9479f1b8c3504b0c9ac652a707a4fc619dcc84
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-30 23:46:32 +0200

    Complete TestMatrix

--- core/src/test/edu/chl/morf/model/TestMatrix.java
+++ core/src/test/edu/chl/morf/model/TestMatrix.java
@@ -14,14 +14,23 @@ public class TestMatrix {
 
 	@Test
 	public void testAddLevelObject() {
+		//Create Matrix with size 5x5
 		Matrix matrix  = new Matrix(5, 5);
+		
+		//Create and add levelObject to a valid position in the Matrix
 		Point2D.Float pos = new Point2D.Float(1, 1);
-		LevelObject object = new LevelObject(TileType.GROUND, pos);
-		matrix.addLevelObject(object);
-		assertTrue(matrix.getLevelObjects().get(matrix.getLevelObjects().size() - 1).equals(object));
+		LevelObject object1 = new LevelObject(TileType.GROUND, pos);
+		matrix.addLevelObject(object1);
+		
+		//Check that the levelObject is now in the Matrix
+		assertTrue(matrix.getLevelObjects().get(0) == object1);
+		
+		//Create and add levelObject to a non-valid position in the Matrix
 		pos.setLocation(10, 10);
-		object = new LevelObject(TileType.GROUND, pos);
-		matrix.addLevelObject(object);
+		LevelObject object2 = new LevelObject(TileType.GROUND, pos);
+		matrix.addLevelObject(object2);
+		
+		//Check that no levelObject was added to the Matrix (size is still 1)
 		assertTrue(matrix.getLevelObjects().size() == 1);
 	}
 
