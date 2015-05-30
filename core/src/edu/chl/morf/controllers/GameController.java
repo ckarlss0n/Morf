d2924a48844053c46c91f97618b99a6b3f567a6a
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-30 13:42:28 +0200

    Add documentation to GameController

--- core/src/edu/chl/morf/controllers/GameController.java
+++ core/src/edu/chl/morf/controllers/GameController.java
@@ -6,57 +6,63 @@ import com.badlogic.gdx.InputAdapter;
 import edu.chl.morf.handlers.KeyBindings;
 
 /**
+ * This class is listening for keystrokes.
+ * As the keyboard bindings are dynamic and changeable, the pressed key is first sent to KeyBindings to see if it matches with any used key.
+ * Depending on which key is pressed, the corresponding instructions are sent to GameLogic as a method call.
+ * <p>
  * Created by Christoffer on 2015-05-06.
  */
-public class GameController extends InputAdapter{
+public class GameController extends InputAdapter {
 
 	private GameLogic gameLogic;
 	private KeyBindings keyBindings = KeyBindings.getInstance();
 
-	public GameController(GameLogic gameLogic){
+	public GameController(GameLogic gameLogic) {
 		this.gameLogic = gameLogic;
 		Gdx.input.setInputProcessor(this);
 	}
 
+	//A key is pressed
 	@Override
 	public boolean keyDown(int keycode) {
 		if (keyBindings.isKey(keycode, keyBindings.getMoveLeftKey())) {
 			gameLogic.setKeyState(keycode, true);
 			gameLogic.moveLeft();
-		} else if (keyBindings.isKey(keycode, keyBindings.getMoveRightKey())){
+		} else if (keyBindings.isKey(keycode, keyBindings.getMoveRightKey())) {
 			gameLogic.setKeyState(keycode, true);
 			gameLogic.moveRight();
-		} else if(keyBindings.isKey(keycode, keyBindings.getJumpKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getJumpKey())) {
 			gameLogic.setKeyState(keycode, true);
 			gameLogic.jump();
-		} else if(keyBindings.isKey(keycode, keyBindings.getFlyKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getFlyKey())) {
 			gameLogic.fly();
-		} else if(keyBindings.isKey(keycode, keyBindings.getPourKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getPourKey())) {
 			gameLogic.placeWater();
-		} else if(keyBindings.isKey(keycode, keyBindings.getHeatKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getHeatKey())) {
 			gameLogic.heatBlock();
-		} else if(keyBindings.isKey(keycode, keyBindings.getCoolKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getCoolKey())) {
 			gameLogic.coolBlock();
-		} else if(keyBindings.isKey(keycode, keyBindings.getPauseKey())){
+		} else if (keyBindings.isKey(keycode, keyBindings.getPauseKey())) {
 			gameLogic.pauseGame();
 		}
 		return true;
 	}
 
+	//A key is released
 	@Override
 	public boolean keyUp(int keycode) {
-		if(keyBindings.isKey(keycode, keyBindings.getMoveLeftKey())) {
+		if (keyBindings.isKey(keycode, keyBindings.getMoveLeftKey())) {
 			gameLogic.setKeyState(Input.Keys.valueOf(keyBindings.getMoveLeftKey()), false);
 			gameLogic.moveLeft();
 			gameLogic.stop();
-		} else if(keyBindings.isKey(keycode, keyBindings.getMoveRightKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getMoveRightKey())) {
 			gameLogic.setKeyState(Input.Keys.valueOf(keyBindings.getMoveRightKey()), false);
 			gameLogic.moveRight();
 			gameLogic.stop();
-		} else if(keyBindings.isKey(keycode, keyBindings.getJumpKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getJumpKey())) {
 			gameLogic.setKeyState(Input.Keys.valueOf(keyBindings.getJumpKey()), false);
 			gameLogic.stop();
-		} else if(keyBindings.isKey(keycode, keyBindings.getFlyKey())) {
+		} else if (keyBindings.isKey(keycode, keyBindings.getFlyKey())) {
 			gameLogic.stop();
 		}
 		return true;
