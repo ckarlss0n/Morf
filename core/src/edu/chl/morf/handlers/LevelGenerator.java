e8f4206a86131055335e4c20a1850d9b1109abb3
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-30 17:29:14 +0200

    Add documentation to LevelGenerator

--- core/src/edu/chl/morf/handlers/LevelGenerator.java
+++ core/src/edu/chl/morf/handlers/LevelGenerator.java
@@ -1,80 +1,70 @@
 package edu.chl.morf.handlers;
 
-import static edu.chl.morf.Constants.LEVEL_PATH;
-import static edu.chl.morf.handlers.Constants.*;
-import static edu.chl.morf.Constants.GROUND_FRICTION;
-
-import java.awt.geom.Point2D;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.ChainShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.World;
-
-import edu.chl.morf.model.Flower;
-import edu.chl.morf.model.Level;
-import edu.chl.morf.model.LevelObject;
-import edu.chl.morf.model.TileType;
-import edu.chl.morf.model.Water;
-import edu.chl.morf.model.WaterState;
+import edu.chl.morf.model.*;
 import edu.chl.morf.userdata.UserData;
 import edu.chl.morf.userdata.UserDataType;
 
+import java.util.Map;
+
+import static edu.chl.morf.Constants.GROUND_FRICTION;
+import static edu.chl.morf.handlers.Constants.*;
 import static edu.chl.morf.handlers.LevelFactory.TILE_SIZE;
 
+/**
+ * This class is used for creating bodies in the world.
+ * It loops through the different level objects contained in the matrix of the level.
+ * The class also creates bodies for the water blocks, and puts them in the bodyBlockMap.
+ * <p>
+ * The class creates fixtures using chain shapes outlining the blocks.
+ * The fixtures are later used for collision detection in the game.
+ */
 public class LevelGenerator {
-	
-	public void generateLevel(Level level, World world, Map<Body, Water> bodyBlockMap){
+
+	public void generateLevel(Level level, World world, Map<Body, Water> bodyBlockMap) {
 
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.fixedRotation = true;
 		FixtureDef fixDef = new FixtureDef();
 		BodyFactory bodyFactory = new BodyFactory();
-		
+		float tileSide = TILE_SIZE/PPM/2;	//Size used when creating fixture shapes
+
 		//Generate ground and spike bodies
-		for (LevelObject object : level.getMatrix().getLevelObjects()){
-			if (object.getTileType() == TileType.GROUND){
+		for (LevelObject object : level.getMatrix().getLevelObjects()) {
+			if (object.getTileType() == TileType.GROUND) {
 				bodyDef.type = BodyType.StaticBody;
-				bodyDef.position.set((object.getPosition().y + 0.5f) * TILE_SIZE / PPM, (object.getPosition().x + 0.5f) * TILE_SIZE / PPM);
+				bodyDef.position.set((object.getPosition().y + 0.5f) * tileSide*2, (object.getPosition().x + 0.5f) * tileSide*2);
 
 				ChainShape chainShape = new ChainShape();
 				Vector2[] v = new Vector2[5];
-				v[0] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
-				v[1] = new Vector2(-TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
-				v[2] = new Vector2(TILE_SIZE / 2 / PPM, TILE_SIZE / 2 / PPM);
-				v[3] = new Vector2(TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
-				v[4] = new Vector2(-TILE_SIZE / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				v[0] = new Vector2(-tileSide, -tileSide); //Bottom left corner of fixture
+				v[1] = new Vector2(-tileSide, tileSide);  //Top left corner of fixture
+				v[2] = new Vector2(tileSide, tileSide);   //Top right corner of fixture
+				v[3] = new Vector2(tileSide, -tileSide);  //Bottom right corner of fixture
+				v[4] = new Vector2(-tileSide, -tileSide); //Bottom left corner of fixture
 
-				chainShape.createChain(v);
+				chainShape.createChain(v); //Create a shape from all vertices
 				fixDef.friction = GROUND_FRICTION;
 				fixDef.shape = chainShape;
 				fixDef.filter.categoryBits = BIT_GROUND;
 				fixDef.filter.maskBits = -1;
 				fixDef.isSensor = false;
 				world.createBody(bodyDef).createFixture(fixDef);
-			}
-			else if (object.getTileType() == TileType.SPIKES){
+			} else if (object.getTileType() == TileType.SPIKES) {
 				bodyDef.type = BodyType.StaticBody;
 				bodyDef.position.set((object.getPosition().y + 0.5f) * TILE_SIZE / PPM, (object.getPosition().x + 0.5f) * TILE_SIZE / PPM);
 
 				ChainShape chainShape = new ChainShape();
 				Vector2[] v = new Vector2[5];
-				v[0] = new Vector2(-TILE_SIZE*0.8f / 2 / PPM, -TILE_SIZE / 2 / PPM);
-				v[1] = new Vector2(-TILE_SIZE*0.8f / 2 / PPM, TILE_SIZE * 0.2f / 2 / PPM);
-				v[2] = new Vector2(TILE_SIZE*0.8f / 2 / PPM, TILE_SIZE * 0.2f/ 2 / PPM);
-				v[3] = new Vector2(TILE_SIZE*0.8f / 2 / PPM, -TILE_SIZE / 2 / PPM);
-				v[4] = new Vector2(-TILE_SIZE*0.8f / 2 / PPM, -TILE_SIZE / 2 / PPM);
+				v[0] = new Vector2(-tileSide*0.8f, -tileSide);      //Bottom left corner of fixture
+				v[1] = new Vector2(-tileSide*0.8f, tileSide*0.2f);	//Top left corner of fixture (make it shorter to match height of spikes)
+				v[2] = new Vector2(tileSide*0.8f, tileSide*0.2f);   //Top right corner of fixture (make it shorter to match height of spikes)
+				v[3] = new Vector2(tileSide*0.8f, -tileSide);       //Bottom right corner of fixture
+				v[4] = new Vector2(-tileSide*0.8f, -tileSide);		//Bottom left corner of fixture
 
-				chainShape.createChain(v);
+				chainShape.createChain(v); //Create a shape from all vertices
 				fixDef.shape = chainShape;
 				fixDef.filter.categoryBits = BIT_SPIKES;
 				fixDef.filter.maskBits = BIT_SENSOR;
@@ -85,24 +75,22 @@ public class LevelGenerator {
 
 		//Generate water bodies
 		for (Water water : level.getWaterBlocks()) {
-			if (water.getState() == WaterState.LIQUID){
+			if (water.getState() == WaterState.LIQUID) {
 				Body body = bodyFactory.createWaterBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
 				bodyBlockMap.put(body, water);
-			}
-			else if (water.getState() == WaterState.SOLID){
+			} else if (water.getState() == WaterState.SOLID) {
 				Body body = bodyFactory.createIceBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
 				bodyBlockMap.put(body, water);
 				bodyBlockMap.get(body).cool();
-			}
-			else if (water.getState() == WaterState.GAS){
+			} else if (water.getState() == WaterState.GAS) {
 				Body body = bodyFactory.createVaporBody(world, new Vector2(water.getPosition().x, water.getPosition().y));
 				bodyBlockMap.put(body, water);
 				bodyBlockMap.get(body).heat();
 			}
 		}
-		
+
 		//Generate flower body
-		if (level.getFlower() != null){
+		if (level.getFlower() != null) {
 			Flower flower = level.getFlower();
 			bodyFactory.createFlowerBody(world, new Vector2(flower.getPosition().x, flower.getPosition().y));
 		}
