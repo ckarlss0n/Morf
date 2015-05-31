7a8af946e53cf0d68b33f741b271b702f12d640e
gurrabz <gustav_m_o_bergstrom@hotmail.com>
2015-05-31 18:13:00 +0200

    Add documentation to Matrix

--- core/src/edu/chl/morf/model/Matrix.java
+++ core/src/edu/chl/morf/model/Matrix.java
@@ -1,43 +1,49 @@
 package edu.chl.morf.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Lage on 2015-05-07.
+ * Class for representing a 2D-matrix with a list.
+ * A Matrix has a number of rows and columns and a list of LevelObjects.
+ * 
+ * @author Lage
  */
 public class Matrix {
 
-    private int maxRows;
-    private int maxColumns;
-    private ArrayList<LevelObject> objects;
+    private int rows;
+    private int columns;
+    private List<LevelObject> objects;
 
-    public Matrix(int maxRows, int maxColumns){
-        this.maxRows = maxRows;
-        this.maxColumns = maxColumns;
+    //Constructors
+    public Matrix(int rows, int columns){
+        this.rows = rows;
+        this.columns = columns;
         objects = new ArrayList<LevelObject>();
     }
-
-    public Matrix(int maxRows, int maxColumns, ArrayList<LevelObject> objects){
+    public Matrix(int maxRows, int maxColumns, List<LevelObject> objects){
         this(maxRows,maxColumns);
         this.objects = objects;
     }
 
+    //Getters
+    public int getRows(){
+    	return rows;
+    }
+    public int getColumns(){
+    	return columns;
+    }
+    public List<LevelObject> getLevelObjects(){
+        return objects;
+    }
+    
+    //Method for adding a LevelObject to the Matrix.
     public void addLevelObject(LevelObject object){
         float x = object.getPosition().x;
         float y = object.getPosition().y;
-        if(x >= 0 && x <= maxRows && y >= 0 && y <= maxColumns){
-            this.objects.add(object);
+        //Check that object position is within matrix size.
+        if(x >= 0 && x <= rows && y >= 0 && y <= columns){
+            objects.add(object);
         }
-    }
-
-    public int getRows(){
-    	return maxRows;
-    }
-    public int getColumns(){
-    	return maxColumns;
-    }
-    
-    public ArrayList<LevelObject> getLevelObjects(){
-        return this.objects;
     }
 }
