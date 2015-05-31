ac1372fadcf67e1584b7b56e8429ed237dc295f4
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-31 13:20:47 +0200

    Disable resizing

--- core/src/edu/chl/morf/main/Main.java
+++ core/src/edu/chl/morf/main/Main.java
@@ -7,34 +7,39 @@ import edu.chl.morf.handlers.HighScoreHandler;
 import edu.chl.morf.handlers.SettingsHandler;
 import edu.chl.morf.screens.ScreenManager;
 
-public class Main extends Game{
+/**
+ * This class starts the game.
+ * Main is created in DesktopLauncher.
+ */
+public class Main extends Game {
 
 	public static final String TITLE = "Morf";
 	public static final int V_WIDTH = 1280;
 	public static final int V_HEIGHT = 720;
 	public static final boolean FULLSCREEN = false;
-	
+	public static final boolean RESIZABLE = false;
+
 	public static final float STEP = 1 / 60f;
 	private float accum;
-	
+
 	private ScreenManager screenManager;
-	
+
 	@Override
 	public void create() {
-        HighScoreHandler.getInstance().load();
-        SettingsHandler.getInstance().load();
+		HighScoreHandler.getInstance().load();
+		SettingsHandler.getInstance().load();
 		screenManager = ScreenManager.getInstance();
 	}
 
-    @Override
-    public void dispose(){
-        HighScoreHandler.getInstance().save();
-        SettingsHandler.getInstance().save();
-    }
-	
-	public void render(){
+	@Override
+	public void dispose() {
+		HighScoreHandler.getInstance().save();
+		SettingsHandler.getInstance().save();
+	}
+
+	public void render() {
 		accum += Gdx.graphics.getDeltaTime();
-		while(accum >= STEP){
+		while (accum >= STEP) {
 			accum -= STEP;
 			screenManager.render(STEP);
 		}
--- desktop/src/edu/chl/morf/desktop/DesktopLauncher.java
+++ desktop/src/edu/chl/morf/desktop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package edu.chl.morf.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import edu.chl.morf.main.Main;
 
 /**
@@ -15,19 +14,19 @@ import edu.chl.morf.main.Main;
  * None.
  * <p>
  * <li><b>Using: </b>
- * <li>{@link edu.chl.morf.Constants}
  * <li>{@link Main}
  * <p>
+ *
  * @author Christoffer Karlsson
  */
 public class DesktopLauncher {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");	//Borderless
 		config.title = Main.TITLE;
 		config.width = Main.V_WIDTH;
 		config.height = Main.V_HEIGHT;
-        config.fullscreen = Main.FULLSCREEN;
+		config.fullscreen = Main.FULLSCREEN;
+		config.resizable = Main.RESIZABLE;
 		new LwjglApplication(new Main(), config);
 	}
 }
