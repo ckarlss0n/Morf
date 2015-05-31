108b1e5de1ac4d4690c3c32e8163ae3f8c2cd243
Christoffer Karlsson <chriskar@student.chalmers.se>
2015-05-31 17:20:11 +0200

    Add documentation to LevelReader

--- core/src/edu/chl/morf/handlers/LevelReader.java
+++ core/src/edu/chl/morf/handlers/LevelReader.java
@@ -5,8 +5,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import com.badlogic.gdx.Gdx;
-
+/**
+ * This class reads all map files (created in Tiled Map Editor) from the levels folder in assets.
+ * The name of the TMX-files from Tiled Map Editor are then placed in the levels ArrayList.
+ * In this way, we have an ArrayList with all the names of the different levels.
+ *
+ * LevelReader is used by HighScoreHandler, in order to get the names of all the levels.
+ */
 public class LevelReader {
 	
 	private static LevelReader instance;
@@ -18,12 +23,13 @@ public class LevelReader {
 		File[] listOfFiles = folder.listFiles();
 		List<String> levels = new ArrayList<String>();
 
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				levels.add(listOfFiles[i].getName());
+		for(File file : listOfFiles){
+			if(file.isFile()){
+				levels.add(file.getName());
 			}
 		}
-        Collections.sort(levels);
+
+        Collections.sort(levels); //Sort the list alphabetically
 		this.levels = levels;
 	}
 	
